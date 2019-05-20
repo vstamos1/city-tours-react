2ff9cdbbe3477decc2789c7e33e4dbee84be792e
Vincent Stamos <vincentstamos@Vincents-MacBook-Pro-2.local>
2019-05-20 13:24:13 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, {Component} from 'react';
+import './App.scss';
+import Navbar from './component/Navbar/Navbar';
+import TourList from './component/TourList/index';
+class App extends Component {
+  render() {
+    return (
+      <>
+      <Navbar></Navbar>
+      <TourList></TourList>
+      </>
+    );
+  }
+  
 }
 
 export default App;
---
+++ src/component/TourList/Tour/Tour.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import './Tour.scss';
+
+export default class Tour extends Component {
+    state = {
+        showInfo: false
+    };
+
+    handleInfo = () => {
+        this.setState ({
+            showInfo: !this.state.showInfo
+        });
+    };
+
+  render() {
+      const {id, city, img, name, info } =  this.props.tour;
+      const { removeTour } = this.props
+    return (
+      <article className="tour">
+      <div className="img-container">
+          <img src={img} alt="tour"/>
+          <span className="close-btn" onClick={() => removeTour(id)}>
+          <i className="fas fa-times-circle"></i>
+          </span>
+      </div>
+
+      <div className="tour-info">
+          <h3>{city}</h3>
+          <h3>{name}</h3>
+          <h5>info {" "} <span onClick={this.handleInfo}>
+          <i className="fas fa-caret-square-down"></i>
+          </span> </h5>
+          {this.state.showInfo && <p> {info}</p>}
+      </div>
+
+      </article>
+    )
+  }
+}
